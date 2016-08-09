5706b30a50268ffe9ae5d95c944bb781cb5b9dc9
mehgellan <baumegg@gmail.com>
2016-08-09 10:29:26 -0700

    attempt to convert one txt file into object with keys

--- ruby_csv_challenge.rb
+++ ruby_csv_challenge.rb
@@ -1,3 +1,32 @@
-File.open("sample/comma.txt").each do |line|
-  p line.split(" ")
+require 'csv'
+samples = Array.new
+samples = CSV.read("sample/comma.txt")
+# sample_two = File.open("sample/space.txt")
+# sample_three = File.open("sample/pipe.txt")
+samples.each('sample/comma.txt') do |line|
+  entry = line
+  entry.map do |item| item.gsub!(/\s/, "") end
+  samples << entry
 end
+
+
+
+
+
+
+
+# parsed_sample_one = sample_one.map do |line|
+#   p line.to_a
+# end
+# p "--------------"
+# p parsed_sample_one
+# parsed_sample_two = sample_two.each do |line|
+#   p line.split(" ")
+# end
+# p "--------------"
+# parsed_sample_three = sample_three.each do |line|
+#   p line.split(" | ")
+# end
+
+# all_samples = parsed_sample_three + parsed_sample_two + parsed_sample_one
+# p all_samples
